b69551a904151d81324e79e011b8114d057836e9
CA <amar.alexandru@gmail.com>
2022-08-30 09:41:38 +0100

    5

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -3,31 +3,34 @@
 include(ROOT_PATH . "/app/database/db.php");
 include(ROOT_PATH . "/app/helpers/validateUser.php");
 
+$errors = array();
 $username = '';
 $email = '';
 $password = '';
 $passwordConf = '';
 
-
-if (isset($_POST['register-btn'])){
+if (isset($_POST['register-btn'])) {
     $errors = validateUser($_POST);
-    
+
     if (count($errors) === 0) {
         unset($_POST['register-btn'], $_POST['passwordConf']);
         $_POST['admin'] = 0;
-        
+
         $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
         $user_id = create('users', $_POST);
-        $users = selectOne('users', ['id' =>$users_id]);
-        
-        dd($users);
-    } else {
-        $username = $_POST['username'];
+        $user = selectOne('users', ['id' => $user_id]);
 
+        dd($user);
+
+    } else{
+        $username = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
         $passwordConf = $_POST['passwordConf'];
+
     }
+
+
+    
 }
-?>
--- register.php
+++ register.php
@@ -24,33 +24,31 @@
 
 <body>
   
-  
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
   <div class="auth-content">
 
     <form action="register.php" method="post">
       <h2 class="form-title">Register</h2>
-
+      
       <?php include(ROOT_PATH . "/app/helpers/formErrors.php"); ?>
-
+      
       <div>
         <label>Username</label>
-        <input type="text" name="username" value="<?php echo $username; ?>" class="text-input" >
+        <input type="text" name="username" value="<?php echo $username; ?>" class="text-input">
       </div>
       <div>
         <label>Email</label>
-        <input type="email" name="email"  value="<?php echo $email; ?>" class="text-input">
+        <input type="email" name="email" value="<?php echo $email; ?>" class="text-input">
       </div>
       <div>
         <label>Password</label>
-        <input type="password" name="password"  value="<?php echo $password; ?>" class="text-input">
+        <input type="password" name="password" value="<?php echo $password; ?>" class="text-input">
       </div>
       <div>
         <label>Password Confirmation</label>
-        <input type="password" name="passwordConf"  value="<?php echo $passwordConf; ?>" class="text-input">
-      </div>
-      <div>
+        <input type="password" name="passwordConf" alue="<?php echo $passwordConf; ?>" class="text-input">
+        <div>
         <button type="submit" name="register-btn" class="btn btn-big">Register</button>
       </div>
       <p>Or <a href="<?php echo BASE_URL . '/login.php' ?>">Sign In</a></p>
